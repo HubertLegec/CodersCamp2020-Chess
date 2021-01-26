2735e37d671dea91305796fd7de3950f5734eb1e
esdawid <starzyk.dawid.0826@gmail.com>
2021-01-26 14:15:05 +0100

    REFACTOR CODE AND TESTS #2

--- src/app/boardDisplay.ts
+++ src/app/boardDisplay.ts
@@ -14,21 +14,21 @@ export class boardDisplay {
         this.createMyFile();
     }
     createChessBoardWindow(): void {
-        let chessBoardWindow: HTMLDivElement = document.createElement('div');
+        const chessBoardWindow: HTMLDivElement = document.createElement('div');
         chessBoardWindow.setAttribute('id', 'chessBoard')
         document.getElementById(this.containerId).appendChild(chessBoardWindow)
     }
     createOpponentFile(): void {
-        let opponentFile: HTMLDivElement = document.createElement('div');
+        const opponentFile: HTMLDivElement = document.createElement('div');
         opponentFile.setAttribute('id', 'opponentFile')
-        let cornerLU: HTMLDivElement = document.createElement('div');
-        let cornerRU: HTMLDivElement = document.createElement('div');
+        const cornerLU: HTMLDivElement = document.createElement('div');
+        const cornerRU: HTMLDivElement = document.createElement('div');
         cornerLU.setAttribute('class', 'corner');
         cornerRU.setAttribute('class', 'corner');
         opponentFile.appendChild(cornerLU);
         let letterASCII: number = 65;
         for (let i: number = 0; i < 8; i++) {
-            let fileBox: HTMLDivElement = document.createElement('div');
+            const fileBox: HTMLDivElement = document.createElement('div');
             fileBox.setAttribute('class', 'file rotate');
             fileBox.textContent = String.fromCharCode(letterASCII);
             opponentFile.appendChild(fileBox);
@@ -38,10 +38,10 @@ export class boardDisplay {
         document.getElementById('chessBoard').appendChild(opponentFile)
     }
     createMyRank(): void {
-        let myRank: HTMLDivElement = document.createElement('div');
+        const myRank: HTMLDivElement = document.createElement('div');
         myRank.setAttribute('id', 'myRank');
         for (let i: number = 8; i > 0; i--) {
-            let rankBox: HTMLDivElement = document.createElement('div');
+            const rankBox: HTMLDivElement = document.createElement('div');
             rankBox.setAttribute('class', 'rank');
             rankBox.textContent = i.toString();
             myRank.appendChild(rankBox)
@@ -49,20 +49,20 @@ export class boardDisplay {
         document.getElementById('chessBoard').appendChild(myRank)
     }
     createBoard(): void {
-        let board: HTMLDivElement = document.createElement('div');
+        const board: HTMLDivElement = document.createElement('div');
         board.setAttribute('id', 'board');
         for (let i: number = 0; i < 64; i++) {
-            let boardBox: HTMLDivElement = document.createElement('div');
+            const boardBox: HTMLDivElement = document.createElement('div');
             boardBox.setAttribute('class', 'box');
             board.appendChild(boardBox);
         }
         document.getElementById('chessBoard').appendChild(board);
     }
     createOpponentRank(): void {
-        let opponentRank: HTMLDivElement = document.createElement('div');
+        const opponentRank: HTMLDivElement = document.createElement('div');
         opponentRank.setAttribute('id', 'opponentRank');
         for (let i: number = 8; i > 0; i--) {
-            let rankBox: HTMLDivElement = document.createElement('div');
+            const rankBox: HTMLDivElement = document.createElement('div');
             rankBox.setAttribute('class', 'rank rotate');
             rankBox.textContent = i.toString();
             opponentRank.appendChild(rankBox)
@@ -70,16 +70,16 @@ export class boardDisplay {
         document.getElementById('chessBoard').appendChild(opponentRank)
     }
     createMyFile(): void {
-        let myFile: HTMLDivElement = document.createElement('div');
+        const myFile: HTMLDivElement = document.createElement('div');
         myFile.setAttribute('id', 'myFile')
-        let cornerLD: HTMLDivElement = document.createElement('div');
-        let cornerRD: HTMLDivElement = document.createElement('div');
+        const cornerLD: HTMLDivElement = document.createElement('div');
+        const cornerRD: HTMLDivElement = document.createElement('div');
         cornerLD.setAttribute('class', 'corner');
         cornerRD.setAttribute('class', 'corner');
         myFile.appendChild(cornerLD);
         let letterASCII: number = 65;
         for (let i: number = 0; i < 8; i++) {
-            let fileBox: HTMLDivElement = document.createElement('div');
+            const fileBox: HTMLDivElement = document.createElement('div');
             fileBox.setAttribute('class', 'file');
             fileBox.textContent = String.fromCharCode(letterASCII);
             myFile.appendChild(fileBox);
