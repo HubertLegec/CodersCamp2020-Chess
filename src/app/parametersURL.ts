fcb852dc165dd2fa9e55e6c5f30b46043a062d08
esdawid <starzyk.dawid.0826@gmail.com>
2021-02-02 21:36:24 +0100

    DISPLAY VALUES FROM LP INTO GAME SETTINGS DIV

--- src/app/parametersURL.ts
+++ src/app/parametersURL.ts
@@ -1,28 +1,21 @@
 export class ParametersURL {
+
     getParams(url: string) {
-        let params = {};
-        const parser = document.createElement('a');
-        parser.href = url;
-        let query = parser.search.substring(1);
-        let vars = query.split('&');
-        for (let i = 0; i < vars.length; i++) {
-            var pair = vars[i].split('=');
-            params[pair[0]] = decodeURIComponent(pair[1]);
-        }
+        let params: Array<string> = [];
+        const paramsUrl = new URLSearchParams(url);
+        paramsUrl.forEach(param => params.push(param));
         return params;
     }
 
-
     displayParameters() {
-        const params = this.getParams(window.location.href);
+        const params = this.getParams(window.location.search);
         const firstPlayerName = document.getElementById('firstPlayerName');
         const firstPlayerTime = document.getElementById('firstPlayerTime');
         const secondPlayerName = document.getElementById('secondPlayerName');
         const secondPlayerTime = document.getElementById('secondPlayerTime');
-        firstPlayerName.textContent = params[0];
-        secondPlayerName.textContent = params[1];
-        firstPlayerTime.textContent = params[2];
-        secondPlayerTime.textContent = params[2];
+        firstPlayerName.innerText = params[0];
+        secondPlayerName.innerText = params[1];
+        firstPlayerTime.innerText = params[2];
+        secondPlayerTime.innerText = params[2];
     }
-
 }
