cfb208c577dbab88e4c05ae4fc1154dc7f81532a
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-02-02 15:02:28 +0100

    Added url building test

--- test/landingPage.test.ts
+++ test/landingPage.test.ts
@@ -1,10 +1,9 @@
-import { LandingPage } from '../src/app/landingPage';
+import { LandingPage } from '../src/app/LandingPage';
 import { fireEvent, getByRole, getByTestId, getByText } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('startingPanelManager', () => {
     let container: HTMLDivElement;
-
 
     function getChessApp(): HTMLDivElement {
         const chessApp = document.createElement('div');
@@ -32,13 +31,14 @@ describe('startingPanelManager', () => {
         document.body.innerHTML = '';
         container = document.body as HTMLDivElement;
         container.append(getChessApp());
-        const landingPage = new LandingPage();
+        const landingPage = new LandingPage('chessGame.html');
         landingPage.addEventsToDOMElements();
     })
 
     test('changes minutes value with slider change', () => {
         const sliderValue = document.getElementById('sliderValue');
         const slider = getByRole(container, 'slider') as HTMLInputElement;
+
         fireEvent.input(slider, { target: { value: 30 } })
 
         expect(getByText(sliderValue, '30')).toBeInTheDocument();
@@ -49,10 +49,24 @@ describe('startingPanelManager', () => {
         const startGameButton: HTMLButtonElement = document.getElementById('gameStartButton') as HTMLButtonElement;
         const firstPlayerInput: HTMLInputElement = document.getElementById('firstPlayerName') as HTMLInputElement;
         const secondPlayerInput: HTMLInputElement = document.getElementById('secondPlayerName') as HTMLInputElement;
+
         fireEvent.input(firstPlayerInput, { target: { value: "Player1" } });
         fireEvent.input(secondPlayerInput, { target: { value: "Player2" } });
 
         expect(startGameButton).not.toHaveAttribute('disabled', "true");
+    })
+
+    test('button link href attribute has set game params', () => {
+        const firstPlayerInput = document.getElementById('firstPlayerName') as HTMLInputElement;
+        const secondPlayerInput = document.getElementById('secondPlayerName') as HTMLInputElement;
+        const slider = getByRole(container, 'slider') as HTMLInputElement;
+        const buttonLink = getByRole(container, 'link') as HTMLAnchorElement;        
+        
+        fireEvent.input(firstPlayerInput, { target: { value: "red" } });
+        fireEvent.input(secondPlayerInput, { target: { value: "blue" } });
+        fireEvent.input(slider, { target: { value: "33"}});
+
+        expect(buttonLink.href).toContain('chessGame.html?name1=red&name2=blue&time=33');
     })
 
 })
