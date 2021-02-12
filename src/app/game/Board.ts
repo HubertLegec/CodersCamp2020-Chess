9eee03f22dc4549623e3ea158e1741c3219652d5
Michal Brejnakowski <mbrejnakowski24@gmail.com>
2021-02-12 21:59:36 +0100

    Change board squares numbering

--- src/app/game/Board.ts
+++ src/app/game/Board.ts
@@ -11,22 +11,26 @@ export class Board {
 
   private initializeBoard() {
     const domSquares = document.getElementsByClassName("box");
-    for (let i = 0; i < 8; i++) {
+    for (let i = 7; i >= 0; i--) {
       this.squares[i] = [];
-      for (let j = 0; j < 8; j++) {
-        this.squares[i][j] = new Square(i, j);
-        this.squares[i][j].domSqare = domSquares[i + 8 * j];
+      for (let j = 7; j >= 0; j--) {
+        this.squares[i][j] = new Square(Math.abs(i - 7), j);
+        this.squares[i][j].setDomSquare(domSquares[Math.abs(i - 7) + 8 * j]);
         //sqare numeration to be changed
-        this.squares[i][j].domSqare.innerHTML = String(i + 8 * j);
+        this.squares[i][j].getDomSquare().innerHTML = String(Math.abs(i - 7) + 8 * Math.abs(j - 7));
       }
     }
-        this.initializePieces();
+    console.log(this.squares)
+    this.initializePieces();
   }
 
   private initializePieces() {
     for (let i = 0; i < 8; i++) {
-      this.squares[i][1].piece = new Pawn(true);
-      this.squares[i][1].domSqare.innerHTML = PieceType.Pawn;
+      this.squares[i][1].setPiece(new Pawn(false));
+      this.squares[i][1].getDomSquare().innerHTML = PieceType.Pawn;
+
+      this.squares[i][6].setPiece(new Pawn(true));
+      this.squares[i][6].getDomSquare(). innerHTML = PieceType.Pawn;
     }
   }
 }
