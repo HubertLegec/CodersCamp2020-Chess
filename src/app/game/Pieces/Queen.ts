d2caa30b18b4dc9454f0c8fcd73f4547ced049d3
Tomasz Massalski <tomasz.massalski@gmail.com>
2021-02-14 16:52:36 +0100

    Queen moves implementation, bishop refactoring

--- src/app/game/Pieces/Queen.ts
+++ src/app/game/Pieces/Queen.ts
@@ -2,7 +2,7 @@ import { Square } from "../Square";
 import { Piece } from "./Piece";
 import { Board } from "../Board";
 
-export class Bishop extends Piece {
+export class Queen extends Piece {
 
   constructor(white: boolean) {
     super(white);
@@ -20,9 +20,20 @@ export class Bishop extends Piece {
     let horizontalDirection = (horizontalDistance == 0)? 0 : ((horizontalDistance/horizontalDistanceDelta));
 
     let distance = (verticalDistanceDelta > 0)? verticalDistanceDelta : horizontalDistanceDelta;
-    
+
     if (to.getPiece() && (to.getPiece().isWhite() == this.isWhite())) {
       return false;
+    }
+
+    if(verticalDistanceDelta * horizontalDistanceDelta == 0){
+        for(let i = 1; i < distance; i++){
+            let checkedSquareV = from.getRow() + (i * verticalDirection);
+            let checkedSquareH = from.getColumn() + (i * horizontalDirection);
+            if(board.getSquares()[checkedSquareV][checkedSquareH].getPiece() != null){
+                return false;
+            }
+        }
+        return true;
     }
 
     if(verticalDistanceDelta - horizontalDistanceDelta == 0 ){
