3ef20c2651582b23f4bacb2e0d89830e36b32486
brudek <brudek@op.pl>
2021-02-15 12:03:56 +0100

    add svg pieces to board

--- src/app/game/Pieces/PieceType.ts
+++ src/app/game/Pieces/PieceType.ts
@@ -1,15 +1,15 @@
 export enum PieceType {
-    White_Pawn = 'WP',
-    White_King = 'WK',
-    White_Queen = 'WQ',
-    White_Rook = 'WR',
-    White_Bishop = 'WB',
-    White_Knight = 'WK',
+    White_Pawn = '<object class="white" data="./img/pieces/Pawn.svg" type="image/svg+xml"></object>',
+    White_King = '<object class="white" data="./img/pieces/King.svg" type="image/svg+xml"></object>',
+    White_Queen = '<object class="white" data="./img/pieces/Queen.svg" type="image/svg+xml"></object>',
+    White_Rook = '<object class="white" data="./img/pieces/Rook.svg" type="image/svg+xml"></object>',
+    White_Bishop = '<object class="white" data="./img/pieces/Bishop.svg" type="image/svg+xml"></object>',
+    White_Knight = '<object class="white" data="./img/pieces/Knight.svg" type="image/svg+xml"></object>',
 
-    Black_Pawn = 'BP',
-    Black_King = 'BK',
-    Black_Queen = 'BQ',
-    Black_Rook = 'BR',
-    Black_Bishop = 'BB',
-    Black_Knight = 'BK',
+    Black_Pawn = '<object class="black" data="./img/pieces/Pawn.svg" type="image/svg+xml"></object>',
+    Black_King = '<object class="black" data="./img/pieces/King.svg" type="image/svg+xml"></object>',
+    Black_Queen = '<object class="black" data="./img/pieces/Queen.svg" type="image/svg+xml"></object>',
+    Black_Rook = '<object class="black" data="./img/pieces/Rook.svg" type="image/svg+xml"></object>',
+    Black_Bishop = '<object class="black" data="./img/pieces/Bishop.svg" type="image/svg+xml"></object>',
+    Black_Knight = '<object class="black" data="./img/pieces/Knight.svg" type="image/svg+xml"></object>'
 }
