dc374e1080309411040d4cf387014de331144860
brudek <brudek@op.pl>
2021-02-15 13:33:19 +0100

    change pieces images type from object to img

--- src/app/game/Pieces/PieceType.ts
+++ src/app/game/Pieces/PieceType.ts
@@ -1,15 +1,15 @@
 export enum PieceType {
-    White_Pawn = '<object class="white" data="./img/pieces/Pawn.svg" type="image/svg+xml"></object>',
-    White_King = '<object class="white" data="./img/pieces/King.svg" type="image/svg+xml"></object>',
-    White_Queen = '<object class="white" data="./img/pieces/Queen.svg" type="image/svg+xml"></object>',
-    White_Rook = '<object class="white" data="./img/pieces/Rook.svg" type="image/svg+xml"></object>',
-    White_Bishop = '<object class="white" data="./img/pieces/Bishop.svg" type="image/svg+xml"></object>',
-    White_Knight = '<object class="white" data="./img/pieces/Knight.svg" type="image/svg+xml"></object>',
+    White_Pawn = '<img class="white" src="./img/pieces/Pawn.svg">',
+    White_King = '<img class="white" src="./img/pieces/King.svg">',
+    White_Queen = '<img class="white" src="./img/pieces/Queen.svg">',
+    White_Rook = '<img class="white" src="./img/pieces/Rook.svg">',
+    White_Bishop = '<img class="white" src="./img/pieces/Bishop.svg">',
+    White_Knight = '<img class="white" src="./img/pieces/Knight.svg">',
 
-    Black_Pawn = '<object class="black" data="./img/pieces/Pawn.svg" type="image/svg+xml"></object>',
-    Black_King = '<object class="black" data="./img/pieces/King.svg" type="image/svg+xml"></object>',
-    Black_Queen = '<object class="black" data="./img/pieces/Queen.svg" type="image/svg+xml"></object>',
-    Black_Rook = '<object class="black" data="./img/pieces/Rook.svg" type="image/svg+xml"></object>',
-    Black_Bishop = '<object class="black" data="./img/pieces/Bishop.svg" type="image/svg+xml"></object>',
-    Black_Knight = '<object class="black" data="./img/pieces/Knight.svg" type="image/svg+xml"></object>'
+    Black_Pawn = '<img class="black" src="./img/pieces/Pawn.svg">',
+    Black_King = '<img class="black" src="./img/pieces/King.svg">',,
+    Black_Queen = '<img class="black" src="./img/pieces/Queen.svg">',
+    Black_Rook = '<img class="black" src="./img/pieces/Rook.svg">',
+    Black_Bishop = '<img class="black" src="./img/pieces/Bishop.svg">',
+    Black_Knight = '<img class="black" src="./img/pieces/Knight.svg">',
 }
